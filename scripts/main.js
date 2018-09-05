36d0d366f3827f9e7895e98a4815046a8cc94553
David Lyons <david.scott.lyons@gmail.com>
2018-09-05 13:06:47 -0700

    interactive threejs header background

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,79 @@
 (function() {
 
-	console.log('hello');
+	var container = document.getElementById( 'three-bg' );
+
+	var scene = new THREE.Scene();
+	scene.background = new THREE.Color( 0xffffff );
+
+	var header = document.querySelector('header');
+	var height = header.offsetHeight;
+
+	var renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, height );
+	container.appendChild( renderer.domElement );
+
+	var camera = new THREE.PerspectiveCamera( 40, window.innerWidth / height, .01, 1000 );
+	camera.position.set( 0, 0, 1.8 );
+
+	// -----------------------------------------------------------------
+
+	var sphereGeo = new THREE.SphereBufferGeometry( 2, 12, 12 );
+	var sphereMat = new THREE.MeshBasicMaterial({
+		color: 0xdddddd,
+		wireframe: true
+	});
+	var sphere = new THREE.Mesh( sphereGeo, sphereMat );
+	scene.add( sphere );
+
+	// -----------------------------------------------------------------
+
+	var mouse = new THREE.Vector2();
+
+	var sphereTarget = new THREE.Euler();
+
+	var xrad = THREE.Math.degToRad(30);
+	var yrad = THREE.Math.degToRad(10);
+
+	header.addEventListener('mousemove', mousemove, false);
+	function mousemove(e){
+		// NDC -1 to 1
+		var rect = renderer.domElement.getBoundingClientRect();
+		mouse.x = ( e.clientX - rect.left ) / rect.width * 2 - 1;
+		mouse.y = ( e.clientY - rect.top ) / rect.height * -2 + 1;
+
+		sphereTarget.y = mouse.x * xrad;
+		sphereTarget.x = - mouse.y * yrad;
+	}
+
+	window.addEventListener( 'resize', resize, false );
+	function resize() {
+		height = header.offsetHeight;
+		camera.aspect = window.innerWidth / height;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, height );
+	}
+
+	renderer.setAnimationLoop( loop );
+
+	function loop() {
+
+		lerp( sphere.rotation, 'x', sphereTarget.x );
+		lerp( sphere.rotation, 'y', sphereTarget.y );
+
+		renderer.render( scene, camera );
+
+	}
+
+	function lerp( object, prop, destination ) {
+		if (object && object[prop] !== destination) {
+			object[prop] += (destination - object[prop]) * 0.1;
+
+			if (Math.abs(destination - object[prop]) < 0.001) {
+				object[prop] = destination;
+			}
+		}
+	}
 
 })();
 
