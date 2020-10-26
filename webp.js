e9c6339bfa4b1beec774a4f768e3262f2f2a2103
David Lyons <david.scott.lyons@gmail.com>
2020-10-26 15:43:25 -0700

    home webp

---
+++ webp.js
@@ -0,0 +1,13 @@
+const imagemin = require('imagemin');
+const webp = require('imagemin-webp');
+
+(async () => {
+	await imagemin(['projects/**/*-thumb.{jpg,png}'], {
+		destination: 'projects/thumbs',
+		plugins: [
+			webp({ quality: 75 })
+		]
+	});
+
+	console.log('Images optimized');
+})();
