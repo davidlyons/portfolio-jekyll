3ff3d2ef7ac80c0f08fcc9f8e7c79cc8f6ef7d7e
David Lyons <david.scott.lyons@gmail.com>
2021-04-05 11:31:39 -0700

    moved 3d modeling, sketches and painting thumbs out of project pages and into new home grid

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,6 +1,6 @@
 (function () {
   var options = {
-    delegate: 'a',
+    // delegate: 'a',
     type: 'image',
     image: {
       cursor: null,
@@ -10,8 +10,11 @@
       enabled: true,
       preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
       navigateByImgClick: true,
-      arrowMarkup:
-        '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir% mfp-chevron"><i class="fas fa-chevron-right fa-3x mfp-prevent-close"></i></button>',
+      arrowMarkup: `<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir% mfp-chevron">
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-chevron-right mfp-prevent-close" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
+      </button>`,
     },
     //http://dimsemenov.com/plugins/magnific-popup/documentation.html#iframe-type
     iframe: {
@@ -25,17 +28,19 @@
     },
     callbacks: {
       open: function () {
-        $('body').addClass('lightbox');
+        $('html').addClass('lightbox');
       },
       close: function () {
-        $('body').removeClass('lightbox');
+        $('html').removeClass('lightbox');
       },
     },
   };
 
-  $('.gallery').magnificPopup(options);
+  $('.home-grid .mfp').magnificPopup(options);
+
+  $('.gallery').magnificPopup({ ...options, delegate: 'a' });
 
   $('.gallery-separate').each(function () {
-    $(this).magnificPopup(options);
+    $(this).magnificPopup({ ...options, delegate: 'a' });
   });
 })();
