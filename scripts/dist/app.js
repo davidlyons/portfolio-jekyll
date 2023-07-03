533c75b1a1a162c5633602be3d2919f615f31c81
David Lyons <david.scott.lyons@gmail.com>
2023-07-02 21:43:28 -0700

    deps

--- scripts/dist/app.js
+++ scripts/dist/app.js
@@ -16,17 +16,23 @@
   renderer.setSize(width, height);
   container.appendChild(renderer.domElement);
   var camera = new THREE.PerspectiveCamera(60, width / height, 0.01, 1000);
-  camera.position.set(0, 0, 2.4); // var aLight = new THREE.AmbientLight(0x080808);
+  camera.position.set(0, 0, 2.4);
+
+  // var aLight = new THREE.AmbientLight(0x080808);
   // scene.add(aLight);
+
   // var dLight = new THREE.DirectionalLight(0xffffff, 1);
   // dLight.position.set(1, 1, 1);
   // scene.add(dLight);
+
   // var dLight = new THREE.DirectionalLight(0xffffff, 0.4);
   // dLight.position.set(-1, -0.25, 0.25);
   // scene.add(dLight);
+
   // var pLight = new THREE.PointLight(0x1ac6ff, 0.8, 10);
   // pLight.position.set(1, 0.5, 0.5);
   // scene.add(pLight);
+
   // -----------------------------------------------------------------
 
   var group = new THREE.Group();
@@ -42,14 +48,15 @@
   var solidMesh = new THREE.Mesh(geo, solidMat);
   group.add(wireMesh);
   group.add(solidMesh);
-  scene.add(group); // -----------------------------------------------------------------
+  scene.add(group);
+
+  // -----------------------------------------------------------------
 
   var mouse = new THREE.Vector2();
   var groupTarget = new THREE.Euler();
   var xRange = THREE.MathUtils.degToRad(90);
   var yRange = THREE.MathUtils.degToRad(180);
   header.addEventListener('mousemove', mousemove, false);
-
   function mousemove(e) {
     // NDC -1 to 1
     var rect = header.getBoundingClientRect();
@@ -57,27 +64,25 @@
     mouse.y = (e.clientY - rect.top) / rect.height * -2 + 1;
     groupTarget.y = mouse.x * yRange;
     groupTarget.x = -mouse.y * xRange;
-  } // window.addEventListener( 'resize', resize, false );
+  }
+
+  // window.addEventListener( 'resize', resize, false );
   // function resize() {
   // 	camera.aspect = width / height;
   // 	camera.updateProjectionMatrix();
   // 	renderer.setSize( width, height );
   // }
 
-
   renderer.setAnimationLoop(loop);
-
   function loop() {
     if (getComputedStyle(renderer.domElement).display == 'none') return;
     lerp(group.rotation, 'x', groupTarget.x);
     lerp(group.rotation, 'y', groupTarget.y);
     renderer.render(scene, camera);
   }
-
   function lerp(object, prop, destination) {
     if (object && object[prop] !== destination) {
       object[prop] += (destination - object[prop]) * 0.1;
-
       if (Math.abs(destination - object[prop]) < 0.001) {
         object[prop] = destination;
       }
